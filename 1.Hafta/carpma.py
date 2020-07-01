5a7444fa19180869d79de63ea685a13fc8ee61c4
Melek Aslan <53269527+melekaslan@users.noreply.github.com>
2020-07-01 22:06:54 +0300

    Add files via upload

---
+++ 1.Hafta/carpma.py
@@ -0,0 +1,7 @@
+def carpma():
+    sayi1 = int(input("Lütfen Birinci Sayıyı Giriniz:"))
+    sayi2 = int(input("Lütfen İkinci Sayıyı Giriniz:"))
+    print("İşlem Sonucunuz:", sayi1 * sayi2)
+
+
+carpma()
